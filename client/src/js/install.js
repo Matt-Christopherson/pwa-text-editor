5319ba6f160e37ffcccd4c4fbe661874261691c1
Matt Christopherson <153093689+Matt-Christopherson@users.noreply.github.com>
2024-05-14 12:30:13 -0500

    Add files via upload

---
+++ client/src/js/install.js
@@ -0,0 +1,20 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+  textHeader.textContent = "Click the button to install!";
+
+  butInstall.addEventListener("click", async () => {
+    event.prompt();
+    butInstall.setAttribute("disabled", true);
+    butInstall.textContent = "Installed!";
+  });
+});
+
+// Handler for the appinstalled event
+window.addEventListener("appinstalled", (event) => {
+  textHeader.textContent = "Successfully installed!";
+  console.log("👍", "appinstalled", event);
+});
